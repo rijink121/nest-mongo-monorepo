a7a9f9aad6315c8b998e3ff3097f01c4409afe8a
Rijin K <rijin@newagesmb.com>
2025-09-17 15:02:28 +0530

    app config moved to core module

--- apps/admin/src/app.module.ts
+++ apps/admin/src/app.module.ts
@@ -1,23 +1,12 @@
 import { CoreModule } from '@core';
-import { AppConfigModule } from '@core/modules/app-config/app-config.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from '@shared';
-import { join } from 'path';
 import { APP_NAME } from './app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [join(process.cwd(), 'apps', APP_NAME, '.env'), '.env'],
-    }),
-    AppConfigModule.register(APP_NAME),
-    CoreModule,
-    SharedModule,
-  ],
+  imports: [CoreModule.register(APP_NAME), SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/core/src/core.module.ts
+++ libs/core/src/core.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 import { CoreService } from './core.service';
 import { AppConfigModule } from './modules/app-config/app-config.module';
 
-@Module({
-  providers: [CoreService],
-  exports: [CoreService],
-  imports: [AppConfigModule],
-})
-export class CoreModule {}
+@Module({})
+export class CoreModule {
+  static register(appName: string): DynamicModule {
+    return {
+      module: CoreModule,
+      imports: [
+        AppConfigModule.register(appName),
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: [join(process.cwd(), 'apps', appName, '.env'), '.env'],
+        }),
+      ],
+      providers: [CoreService],
+      exports: [CoreService],
+    };
+  }
+}
